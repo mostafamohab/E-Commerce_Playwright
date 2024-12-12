61ac6d3be68e04173329b41abec6978361811265
mosta <mostafa_mohab18@hotmail.com>
2024-12-12 11:42:32 +0200

    Update Config File

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,44 +1,51 @@
-import { defineConfig, devices} from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-const browsers = [
-  { name: 'chromium', device: devices['Desktop Chrome'],
-     outputFolder: 'chromium' 
-  },
-  { name: 'firefox', device: devices['Desktop Firefox'],
-     outputFolder: 'firefox' 
-  },
-  { name: 'webkit', device: devices['Desktop Safari'], 
-    outputFolder: 'webkit' 
-  }
-];
+export default defineConfig({
+  workers: 3, // Increase the number of workers for parallel execution
+  
+  // Directory where tests are located
+  testDir: './src/tests',
+  
+  // Global timeout for each test
+  timeout: 60 * 1000,
 
-export default defineConfig ({
-    workers: 3, // Increase the number of workers for parallel execution
-    testDir: './src/tests',
-    timeout: 60 * 1000,
-    use: {
-        baseURL: "https://www.automationexercise.com",
-        headless: false,
-        video: 'on',  // Record video during tests
-        trace: 'off', // This will generte a trace for every test
-        screenshot: 'only-on-failure',
-        launchOptions: {
-          headless: false,
-          slowMo: 50
-        }
+  // Global configuration for all projects
+  use: {
+    baseURL: "https://www.automationexercise.com",
+    headless: false,
+    video: 'on',  // Record video during tests
+    trace: 'off', // Trace generation for debugging
+    screenshot: 'only-on-failure',  // Take screenshot only on failure
+    launchOptions: {
+      headless: false,
+      slowMo: 50  // Slow down the tests to make debugging easier
+    }
+  },
+
+  // Reporter configuration (moved out of projects)
+  reporter: [
+      ['html', { outputFolder: 'test-results/playwright-report' }],
+  ],
+
+  // Static Projects configuration for each browser
+  projects: [
+    {
+      name: 'chromium',
+      use: { 
+        ...devices['Desktop Chrome'],  // Use the device configuration specific to Chromium
+      }
     },
-    
-    projects: 
-        browsers.map((browser) => ({
-        name: 
-        browser.name,
-
-        use: 
-        { ...browser.device, },
-
-    reporter: 
-    [
-          ['html', { outputFolder: `test-results/playwright-report/${browser.outputFolder}` }]
-    ],
-    })),
+    {
+      name: 'firefox',
+      use: { 
+        ...devices['Desktop Firefox'],  // Use the device configuration specific to Firefox
+      }
+    },
+    {
+      name: 'webkit',
+      use: { 
+        ...devices['Desktop Safari'],  // Use the device configuration specific to WebKit (Safari)
+      }
+    },
+  ],
 });
