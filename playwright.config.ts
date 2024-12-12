5dcef95909bd9f92d76cf6625b0196be6ffaf055
mosta <mostafa_mohab18@hotmail.com>
2024-12-12 18:00:40 +0200

    add updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 
   // Reporter configuration (moved out of projects)
   reporter: [
-      ['html', { outputFolder: 'test-results' }],
+      ['html', { outputFolder: 'reports/playwright-report' }],
   ],
 
   // Static Projects configuration for each browser
